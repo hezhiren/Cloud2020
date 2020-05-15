58a0e4cbf95d47f24bdccf0516cbbb424ee8a1a1
hezhiren <hezhiren1994@163.com>
2020-05-15 23:27:00 +0800

    Eureka服务端集群配置

---
+++ cloud-eureka-server7002/src/main/java/cn/hezhiren/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package cn.hezhiren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : hezr
+ * @description :
+ * @date : 2020/05/15
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
